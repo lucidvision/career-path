338686c89e21208b486092c7007d1b5dfa4df7be
Brian Park <brianpark25@gmail.com>
2018-04-05 17:07:10 -0700

    Add results to assessment page

--- src/screens/Assessment/AssessmentScreen.js
+++ src/screens/Assessment/AssessmentScreen.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { Alert, AsyncStorage, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { IconButton } from 'components'
 import { Entypo } from '@expo/vector-icons'
+import _ from 'lodash'
 
 class AssessmentScreen extends Component {
   static navigationOptions = {
     title: 'Career Path'
+  }
+  state = {
+    result: []
+  }
+  async componentDidMount () {
+    try {
+      const result = await AsyncStorage.getItem('assessmentResult')
+      const resultArray = JSON.parse(result)
+      this.setState({
+        result: resultArray
+      })
+    } catch (error) {
+      Alert(error)
+    }
+  }
+  handleResultPressed = () => {
+    this.props.navigation.navigate('Result', {name: 'Results', jobs: this.state.result})
   }
   handleAssessmentPressed = () => {
     this.props.navigation.navigate('Questionnaire')
@@ -13,6 +31,10 @@ class AssessmentScreen extends Component {
   render () {
     return (
       <View style={styles.container}>
+        {!_.isEmpty(this.state.result) &&
+          <TouchableOpacity onPress={this.handleResultPressed} style={styles.card}>
+            <Text>{`You have ${this.state.result.length} job matches!`}</Text>
+          </TouchableOpacity>}
         <IconButton onPress={this.handleAssessmentPressed} title='Start'>
           <Entypo name='text-document' size={25} />
         </IconButton>
@@ -26,6 +48,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
     justifyContent: 'center'
+  },
+  card: {
+    padding: 10,
+    borderColor: 'black',
+    borderRadius: 10,
+    borderWidth: 2,
+    marginBottom: 10
   }
 })
 
